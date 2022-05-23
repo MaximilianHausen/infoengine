3a1f4db08e9e42cfda823faa82ccbe299c4493d0
MaximilianHausen <Max.Ha@directbox.com>
2022-05-23 14:48:06 +0200

    Descriptor set creation

--- src/main/java/net/totodev/infoengine/rendering/vulkan/VkDescriptorHelper.java
+++ src/main/java/net/totodev/infoengine/rendering/vulkan/VkDescriptorHelper.java
@@ -15,13 +15,13 @@ public final class VkDescriptorHelper {
      * @param descriptorCount The amount of descriptors in this binding if this binding is an array, otherwise 1
      * @param shaderStages    Bitmask of the shader stages this binding should be accessible in. See: {@link VK10#VK_SHADER_STAGE_ALL}
      */
-    public record DescriptorBinding(int binding, int descriptorType, int descriptorCount, int shaderStages) {
+    public record DescriptorBindingInfo(int binding, int descriptorType, int descriptorCount, int shaderStages) {
     }
-    public static long createDescriptorSetLayout(VkDevice device, DescriptorBinding... bindings) {
+    public static long createDescriptorSetLayout(VkDevice device, DescriptorBindingInfo... bindings) {
         try (MemoryStack stack = stackPush()) {
             VkDescriptorSetLayoutBinding.Buffer vkBindings = VkDescriptorSetLayoutBinding.calloc(bindings.length, stack);
             for (int i = 0; i < bindings.length; i++) {
-                DescriptorBinding binding = bindings[i];
+                DescriptorBindingInfo binding = bindings[i];
 
                 VkDescriptorSetLayoutBinding vkBinding = vkBindings.get(i);
                 vkBinding.binding(binding.binding);
@@ -70,50 +70,49 @@ public final class VkDescriptorHelper {
         }
     }
 
-    //TODO: Finish createDescriptorSets
-    /*public static MutableLongList createDescriptorSets(VkDevice device, long descriptorPool, long descriptorSetLayout, int amount) {
+    public record BufferRegion(long buffer, long offset, long size) {
+    }
+    public record DescriptorBufferBinding(int binding, int descriptorType, int arrayOffset, BufferRegion... buffers) {
+    }
+    public static long createDescriptorSet(VkDevice device, long descriptorPool, long descriptorSetLayout, DescriptorBufferBinding... bindings) {
         try (MemoryStack stack = stackPush()) {
-            // Useless copies to allocate multiple
-            LongBuffer layouts = stack.mallocLong(amount);
-            for (int i = 0; i < amount; i++)
-                layouts.put(i, descriptorSetLayout);
-
             VkDescriptorSetAllocateInfo allocInfo = VkDescriptorSetAllocateInfo.calloc(stack);
             allocInfo.sType(VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO);
             allocInfo.descriptorPool(descriptorPool);
-            allocInfo.pSetLayouts(layouts);
+            allocInfo.pSetLayouts(stack.longs(descriptorSetLayout));
 
-            LongBuffer pDescriptorSets = stack.mallocLong(amount);
-            if (vkAllocateDescriptorSets(device, allocInfo, pDescriptorSets) != VK_SUCCESS)
+            LongBuffer pDescriptorSet = stack.mallocLong(1);
+            if (vkAllocateDescriptorSets(device, allocInfo, pDescriptorSet) != VK_SUCCESS)
                 throw new RuntimeException("Failed to allocate descriptor sets");
+            long descriptorSet = pDescriptorSet.get(0);
 
-            MutableLongList descriptorSets = LongLists.mutable.empty();
+            VkWriteDescriptorSet.Buffer descriptorWrites = VkWriteDescriptorSet.calloc(bindings.length, stack);
+            for (int i = 0; i < bindings.length; i++) {
+                DescriptorBufferBinding binding = bindings[i];
 
-            VkDescriptorBufferInfo.Buffer bufferInfo = VkDescriptorBufferInfo.calloc(1, stack);
-            bufferInfo.offset(0);
-            bufferInfo.range(UniformBufferObject.SIZEOF);
+                VkDescriptorBufferInfo.Buffer bufferInfos = VkDescriptorBufferInfo.calloc(binding.buffers().length, stack);
+                for (int j = 0; j < binding.buffers().length; j++) {
+                    BufferRegion bufferRegion = binding.buffers()[j];
+                    VkDescriptorBufferInfo bufferInfo = bufferInfos.get(j);
+                    bufferInfo.buffer(bufferRegion.buffer());
+                    bufferInfo.offset(bufferRegion.offset());
+                    bufferInfo.range(bufferRegion.size());
+                }
 
-            VkWriteDescriptorSet.Buffer descriptorWrite = VkWriteDescriptorSet.calloc(1, stack);
-            descriptorWrite.sType(VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET);
-            descriptorWrite.dstBinding(0);
-            descriptorWrite.dstArrayElement(0);
-            descriptorWrite.descriptorType(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
-            descriptorWrite.descriptorCount(1);
-            descriptorWrite.pBufferInfo(bufferInfo);
-
-            for (int i = 0; i < amount; i++) {
-                long descriptorSet = pDescriptorSets.get(i);
-
-                bufferInfo.buffer(uniformBuffers.get(i));
-
+                VkWriteDescriptorSet descriptorWrite = descriptorWrites.get(i);
+                descriptorWrite.sType(VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET);
                 descriptorWrite.dstSet(descriptorSet);
+                descriptorWrite.pBufferInfo(bufferInfos);
 
-                vkUpdateDescriptorSets(device, descriptorWrite, null);
-
-                descriptorSets.add(descriptorSet);
+                descriptorWrite.dstBinding(binding.binding());
+                descriptorWrite.dstArrayElement(binding.arrayOffset());
+                descriptorWrite.descriptorType(binding.descriptorType());
+                descriptorWrite.descriptorCount(binding.buffers().length);
             }
 
-            return descriptorSets;
+            vkUpdateDescriptorSets(device, descriptorWrites, null);
+
+            return descriptorSet;
         }
-    }*/
+    }
 }
